68388687352eb82c593e62c397df111a0eced7df
nainagupta49 <nainaguptaorange@gmail.com>
2020-12-02 15:57:15 +0530

    changes

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+class UserTestCase(TestCase):
+    def test1(self):
+        self.assertEqual('test1', 'test1')
+
+    def test2(self):
+        self.assertEqual('test2', 'test2')
+
+    def test3(self):
+        self.assertEqual('test3', 'test3')
+
+    def test4(self):
+        self.assertEqual('test4', 'test4')
+
+    def test5(self):
+        self.assertEqual('test5', 'test5')
+
+    def test6(self):
+        self.assertEqual('test6', 'test6')
+        
